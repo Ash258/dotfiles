871bc8431260c96d880b3f7b15076a134f5e5e47
Jakub Čábera <cabera.jakub@gmail.com>
2025-10-06 21:09:44 +0200

    feat: Generate completions on change

---
+++ .chezmoiscripts/run_onchange_gen-completion.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# TODO: Support nu
+dest="${HOME}/.zsh/fpath"
+_SH='zsh'
+
+
+function tc() {
+	command -v "$1" &> /dev/null
+}
+
+function gen() {
+	if ! tc "${1}"; then
+		return
+	fi
+
+	"$@" > "${dest}/_${1}.zsh"
+}
+
+gen task --completion "${_SH}"
+gen xh --generate complete-"${_SH}"
+gen ripgrep --generate complete-"${_SH}"
+gen atuin gen-completions --shell "${_SH}"
+gen doggo completions "${_SH}"
+gen rclone completion "${_SH}" -
+
+for tool in \
+	"chezmoi" \
+	"cosign" \
+	"docker" \
+	"helm" \
+	"k6" \
+	"kops" \
+	"kubectl" \
+	"mise" \
+	"polaris" \
+	"sops" \
+	"step" \
+	"usql" \
+	"yq"; do
+	gen "${tool}" completion "${_SH}"
+done
