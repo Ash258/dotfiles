dfc09d25836b53f75bae9f685cbfacb10cc08c5e
Jakub Čábera <cabera.jakub@gmail.com>
2025-10-11 14:37:44 +0200

    feat(atuin): Prevent multiple logins

--- .chezmoiscripts/run_after_setup-atuin.sh
+++ .chezmoiscripts/run_after_setup-atuin.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if atuin status 2>&1 | grep -q 'Username:'; then
+    exit 0
+fi
+
 atuin login \
     --username "$(op read 'op://NEW/4m5rld2glwnxvfrz2f4vmond3y/username' || true)" \
     --password "$(op read 'op://NEW/4m5rld2glwnxvfrz2f4vmond3y/password' || true)" \
