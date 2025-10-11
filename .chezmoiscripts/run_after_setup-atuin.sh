be78e96e778ace62c4bf3703067a5ea10cc36b9b
Jakub Čábera <cabera.jakub@gmail.com>
2025-10-11 13:22:51 +0200

    feat(atuin): Sync automatically

--- .chezmoiscripts/run_after_setup-atuin.sh
+++ .chezmoiscripts/run_after_setup-atuin.sh
@@ -4,3 +4,5 @@ atuin login \
     --username "$(op read 'op://NEW/4m5rld2glwnxvfrz2f4vmond3y/username' || true)" \
     --password "$(op read 'op://NEW/4m5rld2glwnxvfrz2f4vmond3y/password' || true)" \
     --key "$(op read 'op://NEW/4m5rld2glwnxvfrz2f4vmond3y/atuin key' || true)"
+
+atuin sync
