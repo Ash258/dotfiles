f428fee6d29df4432469791e186433183c241bfe
Jakub Čábera <cabera.jakub@gmail.com>
2025-10-11 17:17:12 +0200

    chore: Format spaces

--- .chezmoiscripts/run_onchange_gen-completion.sh
+++ .chezmoiscripts/run_onchange_gen-completion.sh
@@ -5,15 +5,16 @@ dest="${HOME}/.zsh/fpath"
 _SH='zsh'
 
 function tc() {
-	command -v "$1" &> /dev/null
+    command -v "$1" &> /dev/null
 }
 
 function gen() {
-	if ! tc "${1}"; then
-		return
-	fi
+    if ! tc "${1}"; then
+        return
+    fi
 
-	"$@" > "${dest}/_${1}.zsh"
+    echo "Generating completion for '${1}'"
+    "$@" > "${dest}/_${1}.zsh"
 }
 
 gen task --completion "${_SH}"
@@ -24,18 +25,19 @@ gen doggo completions "${_SH}"
 gen rclone completion "${_SH}" -
 
 for tool in \
-	"chezmoi" \
-	"cosign" \
-	"docker" \
-	"helm" \
-	"k6" \
-	"kops" \
-	"kubectl" \
-	"mise" \
-	"polaris" \
-	"sops" \
-	"step" \
-	"usql" \
-	"yq"; do
-	gen "${tool}" completion "${_SH}"
+    "chezmoi" \
+    "cosign" \
+    "docker" \
+    "helm" \
+    "k6" \
+    "kops" \
+    "kubectl" \
+    "limactl" \
+    "mise" \
+    "polaris" \
+    "sops" \
+    "step" \
+    "usql" \
+    "yq"; do
+    gen "${tool}" completion "${_SH}"
 done
