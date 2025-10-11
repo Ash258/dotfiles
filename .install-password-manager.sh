4ee20269624e92028bb2cf8a8c0fca8e95414c26
Jakub Čábera <cabera.jakub@gmail.com>
2025-10-11 12:28:46 +0200

    fix: Make the script noninteractive

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -7,7 +7,7 @@ case "$(uname -s)" in
 Darwin)
     if ! type brew >/dev/null 2>&1; then
         echo 'Installing brew'
-        INTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh || true)" </dev/null ## /dev/null skips pressing enter for the installation
+        NON_INTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh || true)" </dev/null ## /dev/null skips pressing enter for the installation
     fi
     brew install --casks "1password-cli" "1password@beta"
     echo 'Login to 1password and enable the CLI'
