f56545b094eef3f3fd645e0e410e40be288d1841
Jakub Čábera <cabera.jakub@gmail.com>
2025-10-11 10:40:04 +0200

    feat(proxy-forwarder): Convert to alpine

--- dot_config/servers/private_socks5_forwarder/entrypoint.sh
+++ dot_config/servers/private_socks5_forwarder/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 conf='/etc/redsocks.conf'
 envsubst < "${conf}.template" > "${conf}"
@@ -11,4 +11,4 @@ iptables -t nat -A OUTPUT -p tcp --dport 80 -j REDIRECT --to-ports "${REDSOCKS_P
 iptables -t nat -A OUTPUT -p tcp --dport 443 -j REDIRECT --to-ports "${REDSOCKS_PORT}"
 
 # Start Dante server
-danted -f /etc/danted.conf
+sockd -f /etc/danted.conf
