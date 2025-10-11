a8bfe7df35da3786644e79e45978a5fc107d7a8d
Jakub Čábera <cabera.jakub@gmail.com>
2025-10-11 14:01:46 +0200

    feat(brew): Exclude MDM specific packages
    All these apps should be pushed via MDM on work laptop
    
    Add chezmoi itself

--- README.md
+++ README.md
@@ -5,25 +5,6 @@
 1. `xcode-select --install`
 1. `sh -c $"(curl -fsSL dotsetup.ash258.com)"`
    1. script below
-1. Login to 1password
-   1. Get identity file using op
+1. Login to 1password using QR code
+   1. Get identity file using `op`
 1. `bin/chezmoi apply`
-
-```bash
-#!/bin/sh
-
-# TODO: Try to implement the xcode select
-
-sh -c "$(curl -fsLS get.chezmoi.io)"
-~/bin/chezmoi init ash258
-cat <<EOT > "${HOME}/.config/chezmoi/chezmoi.toml"
-encryption = "age"
-
-[age]
-identity = '~/identity.txt'
-recipients = "age1l0e9ted62t864dcf0f5x58rcawwq2prmvs89l9wsyqqz5zxpmelsqh289t"
-
-[hooks.read-source-state.pre]
-    command = ".local/share/chezmoi/.install-password-manager.sh"
-EOT
-```
---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+echo 'Installing chezmoi'
+sh -c "$(curl -fsLS get.chezmoi.io || true)"
+
+echo 'Initializing chezmoi'
+~/bin/chezmoi init ash258
+
+echo 'Creating chezmoi config'
+cat <<EOT > "${HOME}/.config/chezmoi/chezmoi.toml"
+encryption = "age"
+
+[age]
+identity = '~/identity.txt'
+recipients = "age1l0e9ted62t864dcf0f5x58rcawwq2prmvs89l9wsyqqz5zxpmelsqh289t"
+
+[hooks.read-source-state.pre]
+    command = ".local/share/chezmoi/.install-password-manager.sh"
+EOT
+
+mac() {
+# TODO: Test if xcode-select --install is good idea
+    echo 'Configuring macOS'
+}
+
+arch() {
+    echo 'Configuring Arch Linux'
+}
+
+debian() {
+    echo 'Configuring Debian-based Linux'
+}
+
+case "$(uname -s)" in
+Darwin)
+    mac
+    ;;
+Linux)
+    # TODO: Detect and implement based on arch/debian/containers
+    arch
+    debin
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
